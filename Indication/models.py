9d67e1dc128d75236f8d0335bd985f5fe151ec73
GlebDev343 <gleb.wasd@gmail.com>
2023-08-25 03:18:26 +0300

    Some additions

--- Indication/models.py
+++ Indication/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 class Manufacturer(models.Model):
     name = models.CharField(unique=True)
@@ -13,7 +14,7 @@ class MeterModel(models.Model):
         unique_together = ["manufacturer","model_name"]
 
 class MeteringDevice(models.Model):
-    number = models.IntegerField()
+    number = models.IntegerField(max_length=10)
     model_metering_device = models.ForeignKey(MeterModel, on_delete=models.CASCADE)
     date_of_issue = models.DateField()
 
@@ -23,23 +24,25 @@ class MeteringDevice(models.Model):
 class PersonalAccount(models.Model):
     address = models.CharField()
     account_number = models.IntegerField(unique=True)
-    first_name = models.CharField()
-    patronymic = models.CharField()
-    last_name = models.CharField()
-    email = models.CharField(null=True)
-    phone_number = models.IntegerField(null=True)
+    first_name = models.CharField(max_length=50)
+    patronymic = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(null=True, blank=True)
+    phone_number = models.IntegerField(null=True, max_length=10)
+    verification_code = models.TextField()
+    code_validity = models.DateTimeField(default=datetime.now())
 
 class InstalledMeteringDevice(models.Model):
     personal_account = models.ForeignKey(PersonalAccount, on_delete=models.CASCADE)
     metering_device = models.ForeignKey(MeteringDevice, on_delete=models.CASCADE)
     installation_date = models.DateField()
-    remove_date =  models.DateField(null=True)
+    remove_date =  models.DateField(null=True, blank=True)
 
     class Meta:
         unique_together = ["personal_account","metering_device","installation_date"]
 
 class Indication(models.Model):
-    current_value = models.IntegerField()
+    current_value = models.IntegerField(max_length=8)
     time_of_taking = models.DateTimeField()
     metering_device = models.ForeignKey(InstalledMeteringDevice, on_delete=models.CASCADE)
 
