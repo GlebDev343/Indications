2ff93b1206350f70fc3d679c2199c129a4dd7e52
GlebDev343 <gleb.wasd@gmail.com>
2023-10-12 23:33:26 +0300

    flake8, black

--- Indications/celery.py
+++ Indications/celery.py
@@ -5,7 +5,8 @@ from celery import Celery
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Indications.settings')
 
-app = Celery('Indications', backend='redis://localhost', broker='pyamqp://')
+app = Celery('Indications')
+app.conf.broker_url = 'redis://localhost:6379/0'
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
--- telegrambot.py
+++ telegrambot.py
@@ -1,8 +1,9 @@
 import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Indications.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Indications.settings")
 
 import django
+
 django.setup()
 
 import datetime
@@ -17,17 +18,22 @@ from Services.indication_service import save_value, update_code_validity
 token = "6472590444:AAH911J01CgccTCDJ0Wbhb56desyqaqVH2Y"
 bot = telebot.TeleBot(token)
 
-markup2 = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True, one_time_keyboard=True)
+markup2 = types.ReplyKeyboardMarkup(
+    row_width=1, resize_keyboard=True, one_time_keyboard=True
+)
 indication_button = types.KeyboardButton("Indication")
 markup2.add(indication_button)
 
-@bot.message_handler(commands=['start'])
+
+@bot.message_handler(commands=["start"])
 def indication(message):
     bot.send_message(message.chat.id, "Hello", reply_markup=markup2)
 
-@bot.message_handler(commands=['indication'])
+
+@bot.message_handler(commands=["indication"])
 def indication(message):
     bot.send_message(message.chat.id, "Enter your personal account number:")
+
 
 @bot.message_handler(regexp="^\d+$")
 def check_pesonal_account(message):
@@ -37,10 +43,17 @@ def check_pesonal_account(message):
         personal_account = PersonalAccount.objects.get(account_number=account_number)
         if str(personal_account.code_validity) < str(datetime.datetime.now()):
             update_code_validity(account_number)
-        bot.send_message(message.chat.id, "A confirmation code has been sent to your email, please enter the message in the format (indication code)")
+        bot.send_message(
+            message.chat.id,
+            "A confirmation code has been sent to your email, please enter the message in the format (indication code)"
+        )
         print(personal_account.verification_code)
     except PersonalAccount.DoesNotExist:
-        bot.send_message(message.chat.id, "You entered an incorrect account number, please try again:")
+        bot.send_message(
+            message.chat.id,
+            "You entered an incorrect account number, please try again:",
+        )
+
 
 @bot.message_handler(regexp="^\d+ \w+$")
 def check_and_save_record(message):
@@ -51,12 +64,18 @@ def check_and_save_record(message):
             if message.text[i] != " ":
                 current_value += message.text[i]
             else:
-                verification_code += message.text[i+1:]
+                verification_code += message.text[i + 1:]
                 break
-        if PersonalAccount.objects.get(account_number=account_number, verification_code=verification_code).verification_code == verification_code:        
+        if (
+            PersonalAccount.objects.get(
+                account_number=account_number, verification_code=verification_code
+            ).verification_code
+            == verification_code
+        ):
             save_value(cv=current_value, an=account_number)
             bot.send_message(message.chat.id, "Hurray!")
     except PersonalAccount.DoesNotExist:
         bot.send_message(message.chat.id, "Invalid verification code, try again:")
+
 
 bot.infinity_polling()
